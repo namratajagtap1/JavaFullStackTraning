dcfd59caf351e3e41dff25ebc7f76d0773cc0d88
namratajagtap1 <111857212+namratajagtap1@users.noreply.github.com>
2022-08-29 17:29:02 +0530

    Add files via upload

---
+++ oopsasignment_8/Laptop.java
@@ -0,0 +1,10 @@
+package com.yash.oopsasignment_8;
+
+public class Laptop extends Electronic {
+
+	public Laptop(int id, String semiconductorType, String dateOfManufacturing) {
+		super(id, semiconductorType, dateOfManufacturing);
+	
+	}
+
+}
