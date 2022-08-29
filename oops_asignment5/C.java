fc4a97dbadac1d587fdc5ef77140a631761dc21d
namratajagtap1 <111857212+namratajagtap1@users.noreply.github.com>
2022-08-29 17:27:50 +0530

    Add files via upload

---
+++ oops_asignment5/C.java
@@ -0,0 +1,12 @@
+package com.yash.oops_asignment5;
+
+public abstract class C extends B {
+	
+	public abstract int div(int a,int b);
+@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
